c1da7c7051dee8f1e1b3032b2696256c407f45be
ViraBudkivska <budkivska@gmail.com>
2018-06-10 21:05:42 +0300

    Day and month validation;

--- Homework-2/Homework-2/Program.cs
+++ Homework-2/Homework-2/Program.cs
@@ -10,6 +10,33 @@ namespace Homework_2
     {
         static void Main(string[] args)
         {
-        }
+            //Ввести два цілих числа day та month і перевірити чи можуть вони представляти день та місяць.
+            //Вивести true чи false
+            int day;
+            int month;
+            DateTime dt;
+
+            Console.WriteLine("Enter day:");            
+            if (Int32.TryParse(Console.ReadLine(), out day)) { }
+            else
+            {
+                Console.WriteLine("String could not be parsed.");
+            }
+
+            Console.WriteLine("Enter month");
+            if (Int32.TryParse(Console.ReadLine(), out month)) { }
+            else
+            {
+                Console.WriteLine("String could not be parsed.");
+            }
+
+            if (DateTime.TryParse(string.Format("{0}-{1}-{2}", DateTime.Now.Year, month, day), out dt)) { }
+            else
+            {
+                Console.WriteLine("Month and day could not represent datetime.");                
+            }
+            Console.ReadKey();
+
+        }              
     }
 }
